8d6218fd850e69959a6f9f0ac2a7ac2db3ab4f2f
flightcode <flightcode@pm.me>
2022-01-04 21:18:07 +1100

    Add Router to App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Beers } from "./routes";
 import { NavbarWrapper } from "./components";
 
 const App: React.FC = () => (
   <div>
     <NavbarWrapper />
-    <p>BrewMate</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="beers" element={<Beers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
