2e2efe17e344e76b2b29ca0cc8c9340931d52667
flightcode <flightcode@pm.me>
2022-01-04 14:53:06 +1100

    Add Beer route

---
+++ server/routes/beer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/beer', (req, res, next) => {
+  // get placeholder
+});
+
+router.get('/beer/:id', (req, res, next) => {
+  // get placeholder
+});
+
+router.post('/beer', (req, res, next) => {
+  // post placeholder
+});
+
+router.delete('/beer/:id', (req, res, next) => {
+  // delete placeholder
+});
+
+module.exports = router;
