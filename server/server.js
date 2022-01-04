ccbfedf630f94d6e06e7fe27027ad31fe94811fc
flightcode <flightcode@pm.me>
2022-01-04 16:27:23 +1100

    Add mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
-require("dotenv").config({ path: "./.env.development.local" });
-
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 
-const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.error(err)
+);
+
+mongoose.Promise = global.Promise;
 
 app.use(cors());
 app.use(express.json());
 
 app.use(require("./routes/beer"));
  
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(process.env.PORT || 5000, 
+  () => console.log(`Server is running on port: ${port}`)
+);
