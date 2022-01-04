f10f4a251a7deed2c81fecd0b489371acf0859c2
flightcode <flightcode@pm.me>
2022-01-04 21:50:35 +1100

    Update navbar links

--- src/components/NavbarWrapper.tsx
+++ src/components/NavbarWrapper.tsx
@@ -9,13 +9,13 @@ const NavbarWrapper: React.FC = () => (
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <NavDropdown title="Beer" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#beer">All</NavDropdown.Item>
+            <NavDropdown.Item href="/beer">All</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#beer/ale">Ale</NavDropdown.Item>
-            <NavDropdown.Item href="#beer/lager">Lager</NavDropdown.Item>
-            <NavDropdown.Item href="#action/dark">Dark</NavDropdown.Item>
+            <NavDropdown.Item href="/beer/ale">Ale</NavDropdown.Item>
+            <NavDropdown.Item href="/beer/lager">Lager</NavDropdown.Item>
+            <NavDropdown.Item href="/beer/dark">Dark</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link href="#review">Review</Nav.Link>
+          <Nav.Link href="/review">Review</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
