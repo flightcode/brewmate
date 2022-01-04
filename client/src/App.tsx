cf0ef35c0220f8f21d7e4c3ccee642bf0969525b
flightcode <flightcode@pm.me>
2022-01-04 20:53:19 +1100

    Create NavbarWrapper Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import "./App.css";
+import { NavbarWrapper } from "./components";
 
 const App: React.FC = () => (
   <div>
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="#home">BrewMate</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="Beer" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#beer">All</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#beer/ale">Ale</NavDropdown.Item>
-              <NavDropdown.Item href="#beer/lager">Lager</NavDropdown.Item>
-              <NavDropdown.Item href="#action/dark">Dark</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#review">Review</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <NavbarWrapper />
     <p>BrewMate</p>
   </div>
 );
