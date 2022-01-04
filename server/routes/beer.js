3c8819bbb7a500d7ee3deadc0af3b7c636e9bcbe
flightcode <flightcode@pm.me>
2022-01-04 16:04:25 +1100

    Update Beer Route

--- server/routes/beer.js
+++ server/routes/beer.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Beer = require('../models/beer');
 
 router.get('/beer', (req, res, next) => {
-  // get placeholder
+  Beer.find({});
+});
+
+router.get('/beer/type/:type', (req, res, next) => {
+  Beer.find({ type: req.params.type });
 });
 
 router.get('/beer/:id', (req, res, next) => {
-  // get placeholder
+  Beer.findById({ _id: req.params.id });
 });
 
 router.post('/beer', (req, res, next) => {
-  // post placeholder
+  Beer.create(req.body);
 });
 
 router.delete('/beer/:id', (req, res, next) => {
-  // delete placeholder
+  Beer.findByIdAndDelete({ _id: req.params.id });
 });
 
 module.exports = router;
