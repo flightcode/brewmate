6e16207dc7a48f5cb5ad45606a1d0c957358e45b
flightcode <flightcode@pm.me>
2022-01-06 20:00:35 +1100

    Add timestamp options to collection

--- server/models/beer.js
+++ server/models/beer.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const BeerSchema = new Schema({
-  name: { type: String, required: true },
-  brewery: Number,
-  type: { type: String, required: true },
-  hops: [String],
-  malts: [String],
-  abv: Number,
-  ibu: Number,
-});
+const BeerSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    brewery: Number,
+    type: { type: String, required: true },
+    hops: [String],
+    malts: [String],
+    abv: Number,
+    ibu: Number,
+  },
+  { timestamps: true }
+);
 
 const Beer = mongoose.model("beer", BeerSchema);
 
--- server/models/brewery.js
+++ server/models/brewery.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const BrewerySchema = new Schema({
-  name: { type: String, required: true },
-  country: String,
-  website: String,
-});
+const BrewerySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    country: String,
+    website: String,
+  },
+  { timestamps: true }
+);
 
 const Brewery = mongoose.model("brewery", BrewerySchema);
 
