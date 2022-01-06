bd6eab52e554199cd59408a78311416808dcfa26
flightcode <flightcode@pm.me>
2022-01-06 19:25:09 +1100

    Update Beers Component title

--- src/routes/Beers.tsx
+++ src/routes/Beers.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const Beers: React.FC = () => <p>Beers</p>;
+const Beers: React.FC = () => (
+  <div>
+    <Helmet>
+      <title>Beers - BrewMate</title>
+    </Helmet>
+    <p>Beers</p>
+  </div>
+);
 
 export { Beers };
