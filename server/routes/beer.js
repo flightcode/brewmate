0528788355a02b684ef157c0b9f3263d5ef907a4
flightcode <flightcode@pm.me>
2022-01-06 19:51:16 +1100

    Add Beer Controller

--- server/routes/beer.js
+++ server/routes/beer.js
@@ -1,41 +1,20 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
-const Beer = require('../models/beer');
+const beer = require("../controllers/beer");
 
-router.get('/beer', (req, res, next) => {
-    Beer.find({})
-    .then((data) => res.json(data))
-    .catch(next);
-});
+router.get("/beer", beer.getAll);
 
-router.get('/beer/:id', (req, res, next) => {
-  Beer.findById({ _id: req.params.id })
-    .then((data) => res.json(data))
-    .catch(next);
-});
+router.get("/beer/:id", beer.getById);
 
-router.get('/beer/type/:type', (req, res, next) => {
-  Beer.find({ type: req.params.type })
-    .then((data) => res.json(data))
-    .catch(next);
-});
+// router.get('/beer/type/:type', (req, res, next) => {
+//   Beer.find({ type: req.params.type })
+//     .then((data) => res.json(data))
+//     .catch(next);
+// });
 
-router.post('/beer', (req, res, next) => {
-  if(req.body.name && req.body.type) {
-    Beer.create(req.body)
-      .then((data) => res.json(data))
-      .catch(next);
-  } else {
-      res.json({
-          error: "Required fields are empty"
-      });
-  }
-});
+router.post("/beer", beer.add);
 
-router.delete('/beer/:id', (req, res, next) => {
-  Beer.findByIdAndDelete({ _id: req.params.id })
-    .then((data) => res.json(data))
-    .catch(next);
-});
+router.delete("/beer/:id", beer.remove);
 
 module.exports = router;
