3ef94dd02852dc28419975c5aaf782a960898d82
flightcode <flightcode@pm.me>
2022-01-06 18:12:04 +1100

    Fix bootstrap navbar with router links

--- src/components/NavbarWrapper.tsx
+++ src/components/NavbarWrapper.tsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavbarWrapper: React.FC = () => (
   <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
     <Container>
-      <Navbar.Brand href="#home">BrewMate</Navbar.Brand>
+      <LinkContainer to="/">
+        <Navbar.Brand>BrewMate</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <NavDropdown title="Beer" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/beer">All</NavDropdown.Item>
+            <LinkContainer to="/beer">
+              <NavDropdown.Item>All</NavDropdown.Item>
+            </LinkContainer>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/beer/ale">Ale</NavDropdown.Item>
-            <NavDropdown.Item href="/beer/lager">Lager</NavDropdown.Item>
-            <NavDropdown.Item href="/beer/dark">Dark</NavDropdown.Item>
+            <LinkContainer to="/beer/ale">
+              <NavDropdown.Item>Ale</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/beer/lager">
+              <NavDropdown.Item>Lager</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/beer/dark">
+              <NavDropdown.Item>Dark</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
-          <Nav.Link href="/review">Review</Nav.Link>
+          <LinkContainer to="/review">
+            <Nav.Link>Review</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Container>
