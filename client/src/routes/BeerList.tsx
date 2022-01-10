0dac592ef97efd5599fe2ba63637abab6db79e96
flightcode <flightcode@pm.me>
2022-01-10 18:24:22 +1100

    Update Card display

--- client/src/routes/BeerList.tsx
+++ client/src/routes/BeerList.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet";
+import { Row, Col } from "react-bootstrap";
 import { BeerCard } from "../components/BeerCard";
 
 interface Beer {
+  _id: string;
   name: string;
   brewery: string;
   type: string;
@@ -16,22 +18,39 @@ interface Beer {
 const BeerList: React.FC = () => {
   const [beers, setBeers] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/beer")
+      .then((res) => {
+        if (res.data) {
+          setBeers(res.data);
+          console.log(res.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [""]);
+
   return (
     <div>
       <Helmet>
         <title>Beers - BrewMate</title>
       </Helmet>
-      {beers.map((beer: Beer) => (
-        <BeerCard
-          name={beer.name}
-          brewery={beer.brewery}
-          type={beer.type}
-          hops={beer.hops}
-          malts={beer.malts}
-          abv={beer.abv}
-          ibu={beer.ibu}
-        />
-      ))}
+      <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+        {beers.map((beer: Beer) => (
+          <Col>
+            <BeerCard
+              key={beer._id}
+              name={beer.name}
+              brewery={beer.brewery}
+              type={beer.type}
+              hops={beer.hops}
+              malts={beer.malts}
+              abv={beer.abv}
+              ibu={beer.ibu}
+            />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
