8d3f79a10f025dadf6668952e8a730ae04b0b4ed
flightcode <flightcode@pm.me>
2022-01-10 18:32:14 +1100

    Update Navbar margin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import { Home, BeerList } from "./routes";
 import { NavbarWrapper } from "./components";
 
 const App: React.FC = () => (
   <div>
     <NavbarWrapper />
-    <Routes>
-      <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="beer" element={<BeerList />} />
-      </Route>
-    </Routes>
+    <Container className="mt-4">
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="beer" element={<BeerList />} />
+        </Route>
+      </Routes>
+    </Container>
   </div>
 );
 
