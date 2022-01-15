f5835d89d954f0549e7f9e4a4d5ddc84f73e9149
flightcode <flightcode@pm.me>
2022-01-15 18:30:08 +1100

    Fix Authorization header parsing

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers.Authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(422).json({ errors: [{ token: "required" }] });
