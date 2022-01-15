d08537cac8c638dbab788d8770ce9abcb607c870
flightcode <flightcode@pm.me>
2022-01-15 14:17:14 +1100

    Add Register Route

--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -1,5 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { api } from "../utils";
 
-const Register: React.FC = () => <p>Register</p>;
+const Register: React.FC = () => {
+  const navigate = useNavigate();
+
+  const register = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+      email: { value: string };
+      password: { value: string };
+    };
+
+    const user = {
+      name: target.name.value,
+      email: target.email.value,
+      password: target.email.value,
+    };
+
+    api
+      .post("/user/register", {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      })
+      .then((res) => {
+        if (res) {
+          navigate("/dashboard");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    api
+      .get("/user/isAuth")
+      .then((res) => {
+        if (res.data.username) {
+          navigate("/dashboard");
+        }
+      })
+      .catch((err) => console.error(err));
+  }, [""]);
+  return (
+    <div>
+      <p>Register</p>
+      <Form onSubmit={(event) => register(event)}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="name"
+            placeholder="Username"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            placeholder="Email address"
+          />
+          <Form.Text className="text-muted">
+            We&apos;ll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            required
+            type="checkbox"
+            label="I agree to the Privacy Policy."
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export { Register };
