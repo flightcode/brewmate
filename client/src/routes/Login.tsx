3bfe3f0bb9747190016117ab8f120fa8aa319fbf
flightcode <flightcode@pm.me>
2022-01-15 18:12:46 +1100

    Update Login Route

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { api } from "../utils";
 
-const Login: React.FC = () => <p>Login</p>;
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const login = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      email: { value: string };
+      password: { value: string };
+    };
+
+    const user = {
+      email: target.email.value,
+      password: target.password.value,
+    };
+
+    api
+      .post("/user/login", {
+        email: user.email,
+        password: user.password,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          localStorage.setItem("token", res.data.token);
+          navigate("/dashboard");
+        }
+      });
+  };
+
+  useEffect(() => {
+    api.get("/user/isAuth").then((res) => {
+      if (res.status === 200) {
+        navigate("/dashboard");
+      }
+    });
+  });
+  return (
+    <div>
+      <p>Log In</p>
+      <Form onSubmit={(event) => login(event)}>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            placeholder="Email address"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export { Login };
