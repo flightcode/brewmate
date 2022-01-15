cd298c5d004730c5ba1871498d6675c1715a91ab
flightcode <flightcode@pm.me>
2022-01-15 18:37:09 +1100

    Create Dashboard & PrivacyPolicy Routes

---
+++ client/src/routes/PrivacyPolicy.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PrivacyPolicy: React.FC = () => <p>Privacy Policy</p>;
+
+export { PrivacyPolicy };
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,4 +1,6 @@
 export * from "./Home";
+export * from "./PrivacyPolicy";
 export * from "./BeerList";
 export * from "./Login";
 export * from "./Register";
+export * from "./Dashboard";
