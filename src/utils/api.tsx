33d821797f01f62b64054d3b2b99b08722b536cd
flightcode <flightcode@pm.me>
2022-01-15 18:30:18 +1100

    Fix Authorization header refresh

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -3,9 +3,19 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
   responseType: "json",
-  headers: {
-    Authorization: `${localStorage.getItem("token")}`,
-  },
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export { api };
