0280151d8883245f82a336cb4b4616f67f796560
flightcode <flightcode@pm.me>
2022-01-15 18:15:07 +1100

    Update Authorization token naming

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:5000/api",
   responseType: "json",
   headers: {
-    "x-access-token": `${localStorage.getItem("token")}`,
+    Authorization: `${localStorage.getItem("token")}`,
   },
 });
 
