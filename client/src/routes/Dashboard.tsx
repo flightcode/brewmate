fae1404ac62262ca4fc7f95e95548459d8c78e7c
flightcode <flightcode@pm.me>
2022-01-16 17:02:08 +1100

    Update Helmet titles

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const Dashboard: React.FC = () => <p>Dashboard</p>;
+const Dashboard: React.FC = () => (
+  <div>
+    <Helmet>
+      <title>Dashboard - BrewMate</title>
+    </Helmet>
+    <p>Dashboard</p>
+  </div>
+);
 
 export { Dashboard };
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const Home: React.FC = () => <p>BrewMate</p>;
+const Home: React.FC = () => (
+  <div>
+    <Helmet>
+      <title>BrewMate — Explore Beers & Track Tasting</title>
+    </Helmet>
+    <p>BrewMate</p>
+  </div>
+);
 
 export { Home };
