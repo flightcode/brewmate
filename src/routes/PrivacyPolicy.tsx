ca416a8d0695e5a06645caaa5c04e78316c41a79
flightcode <flightcode@pm.me>
2022-01-16 17:02:08 +1100

    Update Helmet titles

--- src/routes/PrivacyPolicy.tsx
+++ src/routes/PrivacyPolicy.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const PrivacyPolicy: React.FC = () => <p>Privacy Policy</p>;
+const PrivacyPolicy: React.FC = () => (
+  <div>
+    <Helmet>
+      <title>Privacy Policy - BrewMate</title>
+    </Helmet>
+    <p>Privacy Policy</p>
+  </div>
+);
 
 export { PrivacyPolicy };
