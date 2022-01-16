518d5439d31e80807845eeaa62cab9c6464dd3f2
flightcode <flightcode@pm.me>
2022-01-16 13:19:40 +1100

    Remove Beer search function

--- server/controllers/beer.js
+++ server/controllers/beer.js
@@ -6,19 +6,6 @@ exports.getAll = (req, res, next) => {
     .catch(next);
 };
 
-exports.search = (req, res, next) => {
-  const searchTerm = req.params.search;
-
-  Beer.find({
-    $or: [
-      { name: { $regex: `${searchTerm}` } },
-      { type: { $regex: `${searchTerm}` } },
-    ],
-  })
-    .then((data) => res.json(data))
-    .catch(next);
-};
-
 exports.getById = (req, res, next) => {
   Beer.findById({ _id: req.params.id })
     .then((data) => res.json(data))
--- server/routes/beer.js
+++ server/routes/beer.js
@@ -3,15 +3,7 @@ const express = require("express");
 const router = express.Router();
 const Beer = require("../controllers/beer");
 
-router.get("/beers", Beer.getAll);
-router.get("/beers/:search", Beer.search);
-
-// router.get('/beer/type/:type', (req, res, next) => {
-//   Beer.find({ type: req.params.type })
-//     .then((data) => res.json(data))
-//     .catch(next);
-// });
-
+router.get("/beer", Beer.getAll);
 router.get("/beer/:id", Beer.getById);
 router.post("/beer", Beer.add);
 router.delete("/beer/:id", Beer.remove);
