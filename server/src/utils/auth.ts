26782f82f8ba24fd73272d4e896341f211742751
flightcode <flightcode@pm.me>
2022-06-18 16:23:27 +1000

    Update verifyToken error codes

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -9,16 +9,19 @@ export type AuthenticatedRequest = Request & {
 export function verifyToken(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization?.split(" ")[1];
 
+  // Check token exists
   if (!token) {
-    return res.status(422).json({ errors: [{ token: "invalid" }] });
+    return res.status(401).json({ token: "invalid" });
   }
 
+  // Verify JWT
   jwt.verify(
     token,
     process.env.TOKEN_SECRET,
     (err, decoded: { id: string }) => {
+      // Check JWT valid
       if (err || !decoded.id) {
-        return res.status(422).json({ errors: [{ token: "invalid" }] });
+        return res.status(401).json({ token: "invalid" });
       }
 
       const authReq = req as AuthenticatedRequest;
